91515b2b5cb8f84048f17f3d93560dd4bc02909e
Renato Eliasy <renatoelisasy@gmail.com>
2024-12-06 19:50:29 +0100

    creation of the structure for the project as the modulization of all the dir inside it

---
+++ Main_Folder/Results/CSVRes/__init__.py
@@ -0,0 +1,7 @@
+"""Copyrigth (c) 2024 by R. Eliasy
+
+Author: Renato Eliasy
+Contact: renatoeliasy@gmail.com
+Date: 06-12-2024
+
+Introduction of the __init__.py file to recognize the directory as a module"""
