61a634058ccc936a860185a43cf3f3d8031abd6f
Renato Eliasy <renatoelisasy@gmail.com>
2026-07-19 16:21:20 +0200

    setup the logging function in the right module

---
+++ Main_Folder/Modules/configuration_setting/logger_configuration.py
@@ -0,0 +1,47 @@
+import logging
+from pathlib import Path
+from typing import Optional, Union
+
+def set_logger(
+    level: int = logging.INFO,
+    log_file: Optional[Union[str, Path]] = None
+) -> logging.Logger:
+    '''
+    a function to set the logger and logging for each file/script, it has to be called at the start of each script if used:
+    the LEVEL OF PRIORITY: DEBUG < INFO < WARNING < ERROR < CRITICAL. As stated logging.DEBUG prints also logging.INFO, 
+    but logging.WARNING doesn't print logging.DEBUG
+
+    Args:
+        level (int, optional): Define the level of verbosity. Defaults to logging.INFO.
+        log_file (str | None, optional): The log file where most/all of the log will be saved. Defaults to None.
+
+    Returns:
+        logging.Logger: to be used as log_for_this_file = set_logger(..) and then log_for_this_file.DEBUG('message')
+    '''
+    
+    # reset (importante nei notebook)
+    for h in logging.root.handlers[:]:
+        logging.root.removeHandler(h)
+
+    logger = logging.getLogger(__name__)
+    logger.handlers.clear()
+    logger.setLevel(level)
+
+    formatter = logging.Formatter(
+        '[%(levelname)s] %(name)s - %(message)s'
+    )
+
+    # console
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    # file opzionale
+    if log_file is not None:
+        fh = logging.FileHandler(log_file)
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+    return logger
