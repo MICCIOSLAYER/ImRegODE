8189eac941665d17c2c18dd79b340fac6a88b9e5
Renato Eliasy <renatoelisasy@gmail.com>
2026-08-07 15:02:46 +0200

    creation of the final script for registration algorithm

---
+++ Main_Folder/Modules/framework/registration/algorithms.py
@@ -0,0 +1,34 @@
+# all registration algorithms, it contains the complete wraps of registration, 
+# final products registration that give out result to be collected
+
+
+
+
+
+
+
+
+
+
+
+#                                    =================================
+#                                            SITK REGISTRATION
+#                                    =================================
+
+
+
+#                                    =================================
+#                                           ELASTIX REGISTRATION
+#                                    =================================
+
+
+
+#                                    =================================
+#                                            AIRLAB REGISTRATION
+#                                    =================================
+
+
+
+#                                    =================================
+#                                            DRMINE REGISTRATION
+#                                    =================================
