aeb381512076b006a1c9b2c585d23701c5944144
Renato Eliasy <renatoelisasy@gmail.com>
2026-08-11 19:58:01 +0200

    added naed evaluation

--- Main_Folder/Modules/framework/evaluations.py
+++ Main_Folder/Modules/framework/evaluations.py
@@ -2,10 +2,13 @@
 # it contains also the utils for these methods
 from Main_Folder.Modules.framework.data_classes import SampleDict
 from Main_Folder.Modules.configuration_setting.yaml_configuration import  FrameworkConfig
-from typing import Optional, Union
+from Main_Folder.Modules.configuration_setting.logger_configuration import get_logger
+from typing import Optional, Union, Literal
+import numpy as np
+import torch
 from pathlib import Path
 
-
+standard_log = get_logger(__name__)
 
 
 def get_coords(image_name : str | SampleDict , #es A01
@@ -35,3 +38,81 @@ def get_coords(image_name : str | SampleDict , #es A01
             reference_points.append([float(ref_x), float(ref_y)])
             test_points.append([float(test_x), float(test_y)])
     return test_points, reference_points
+
+
+def naed_evaluation(image_couple_name: str,
+                    affine_matrix: np.ndarray | torch.Tensor, 
+                    image_normalization :  Literal['diagonal' , 'coords_norm'] = 'coords_norm',
+                    control_points_path: Path = FrameworkConfig().path_dict['CONTROL_POINTS_FOLDER'],
+                    image_dimensions: tuple | Path = (2912, 2912) ,
+                    )-> float:
+    '''
+    Evaluate the registration using Normalized Average Euclidean Distance (NAED) metric.
+    Parameters
+    ----------
+    image_couple_name : str
+        The name of the image couple to get the control points file from the list. It has to be in the format 'A01', 'P19', 'S20', etc.
+    control_points_file : Path
+        The path to the control points coordinates.
+    affine_matrix : np.ndarray | torch.Tensor
+        The affine transformation matrix get by image registration algoritm it has to be in the complete form : | A t |
+                                                                                                                | 0 a |
+    image_dimensions : tuple | Path
+        The dimensions of the images or the path to the image to get the dimensions to normalize.
+    '''
+    h,w = image_dimensions
+    
+    #1. get the control points for the image couple
+    test_points_list, reference_points_list = get_coords(
+        image_name=image_couple_name,
+        ground_truth_path=control_points_path
+    )
+
+    test_points = np.array(test_points_list, dtype=np.float32)  # shape (N, D)  D = 2 
+    reference_points = np.array(reference_points_list, dtype=np.float32)  # shape (N, D)
+
+    assert test_points.shape == reference_points.shape , "Test and reference points must have the same shape"
+
+    #2. control of affine matrix? for 2/3D or affine type? NOTE for the moment it has just to be in a quadratic form NxN
+    if isinstance(affine_matrix, torch.Tensor):
+        affine_matrix = affine_matrix.detach().cpu().numpy()
+    affine_matrix = np.asarray(affine_matrix, dtype=np.float32)
+    n, m = affine_matrix.shape
+    if n != m:
+        raise ValueError(f'The affine matrix must be square, got {n}x{m}')
+    
+    if n == 4:
+        total_distance = np.nan # NOTE the 3D case has yet to be treated
+        standard_log.info('the 3D case has yet to be treated')
+        return total_distance
+    
+    elif n == 3:
+        #3. for each list of coords: apply the matrix -> get the Euclidean distance
+        total_distance = 0.0
+        assert test_points.shape[-1] == 2, "Test points must be 2D coordinates"
+        ones_column = np.ones((test_points.shape[0], 1), dtype=np.float32)  # shape (N, 1)
+        homogeneous_test_points = np.hstack((test_points, ones_column))  # shape (N, 3
+        homogeneous_warped_points = homogeneous_test_points @ affine_matrix.T  # shape (N, 3), already interpolated, using round/nearest neighbor
+        
+        incongruencies = [w for w in homogeneous_warped_points[:, 2] if w == 0]
+        if len(incongruencies) > 0:
+            total_distance = np.nan #raise ValueError("There are some incongruencies in this transformation, some points have w=0") #FIXME return NaN o gestisci in altro modo
+            return total_distance
+        
+        else: 
+            warped_points = np.rint([[x_w/w, y_w/w] for (x_w, y_w, w) in homogeneous_warped_points]).astype(np.int32)  # shape (N, 2)
+            total_distance += np.linalg.norm(warped_points - reference_points, axis=1).mean()
+
+    #4. normalize the NAED value
+    if image_normalization == 'diagonal':
+        #4.1 get the NAED value using image diagonal to normalize
+        ratio = np.sqrt(h**2 + w**2)
+    elif image_normalization == 'coords_norm':
+        #4.2 get the NAED value using the norm of the image dimensions to normalize
+        ratio = h
+        
+    #5. return NAED value in float
+    naed_value = total_distance / ratio
+    
+    return naed_value
+    
