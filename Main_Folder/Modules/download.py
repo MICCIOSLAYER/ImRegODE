880ac8eb635583d9a3014de34886a4f0cf5c30ea
Renato Eliasy <renatoelisasy@gmail.com>
2026-08-28 17:11:02 +0200

    download scripts from github

--- Main_Folder/Modules/download.py
+++ Main_Folder/Modules/download.py
@@ -4,9 +4,11 @@ import zipfile
 import py7zip as py7z
 import os
 import sys
-
+from Main_Folder.Modules.configuration_setting.logger_configuration import get_logger
 from Main_Folder.configparser import Config
+from Main_Folder.Modules.utils  import get_root_path
 
+standard_log = get_logger(__name__)
 config_file_path = Path(__file__).resolve().parents[2] / "config.txt"
 config = Config(config_file_path)
 links = config.get_link()
@@ -32,11 +34,22 @@ def download_file(
     None
 
     """
-
-    pass
+    try:
+        with open(file_path, 'r') as f:
+            standard_log.warning(f'the {file_path} already exists')
+    except FileNotFoundError:
+        request = requests.get(url=url)
+        if not request.ok:
+            standard_log.error(f'{url} got a problem due to {request.status_code} code')
+        else:
+            with open(file_path, 'wb') as f:
+                f.write(request.content)
+    return
 
 def import_trial():
     print('successful')
     return None
+
+
 if __name__ == "__main__":
     print(config_file_path)
