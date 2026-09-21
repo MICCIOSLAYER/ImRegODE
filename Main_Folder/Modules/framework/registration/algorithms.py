decde0122e9b710029fa90682ed7516be08a1a5f
Renato Eliasy <renatoelisasy@gmail.com>
2026-09-21 18:17:07 +0200

    adj islice positional argument assegnation

--- Main_Folder/Modules/framework/registration/algorithms.py
+++ Main_Folder/Modules/framework/registration/algorithms.py
@@ -1047,7 +1047,7 @@ def results_collection(
     registration_name = registration_name or registration_fn.__name__
 
     if max_sample is not None and 0 < max_sample <= len(dataloader):
-        data_iterator = islice(iterable = data_iterator, stop = max_sample)
+        data_iterator = islice(data_iterator, max_sample)
     for sample_set in data_iterator:
         preprocessed_sample_dict = preprocessing_fn(sample_dict=sample_set, config_dict=config_dict)
         registration_results = registration_fn(preprocessed_sample_dict, config_dict=config_dict)
